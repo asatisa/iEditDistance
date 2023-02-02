cd47873da875001b8fae3bb0d2b536d076e6712e
asatisa <asatisa.wang@gmail.com>
2023-02-03 06:11:02 +0700

    Update

--- cseditdistance/Program.cs
+++ cseditdistance/Program.cs
@@ -1,11 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using cseditdistance;
 using System.Xml.Linq;
 
 //var name = Console.ReadLine();
 var currentDate = DateTime.Now;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Calculation of Edit Distance! version.0.5");
 Console.WriteLine($"{Environment.NewLine}On {currentDate:d} at {currentDate:t}!");
 
-Console.Write($"{Environment.NewLine}Press any key to exit...");
-Console.ReadKey(true);
+RunTest r = new RunTest();
+
+
+int result = 0;
+
+//string text1 = "ABBYY";
+//string text2 = "ABBY";
+
+//result = LevenshteinDistance.Calculate(text1, text2);
+
+//Console.WriteLine($"Calculation : text1 = {text1} , text2 = {text2} : Return value = {result}");
+result = r.Calculate("ABBYYYY", "ABBY");
+result = r.Calculate("hospital", "haspita");
+result = r.Calculate("haspita", "hospital");
+result = r.Calculate("1.เงินกู้สวัสดิการแก่ข้าราชการ การเมืองท้องถิ่นสังกัด อบต. อบจ. และเทศบาล", "1.เงินกู้สวัสดิการแก่ข้าราชการ การเมืองท้องถิ่นสังกัด อบต. อบจ. และเทศบาล");
+result = r.Calculate("2.เงินกู้สวัสดิการแก่ข้าราชการ การเมืองท้องถิ่นสังกัด อบต. อบจ. และเทศบาล", "2..เงินกสฺ.วัสุดฺ.ทาร.แก่ข้าราชการ.กา.รเ.มีองฺ.ท้องถนสุงฺ.กู้^.ชินร^.แส?เทศบาล");
+result = r.Calculate(".เงินกสฺ.วัสุดฺ.ทาร.แก่ข้าราชการ.กา.รเ.มีองฺ.ท้องถนสุงฺ.กู้^.ชินร^.แส?เทศบาล","เงินกู้สวัสดิการแก่ข้าราชการ การเมืองท้องถิ่นสังกัด อบต. อบจ. และเทศบาล");
+result = r.Calculate("3.เงินกู้สวัสดิการแก่ข้าราชการ", "3..เงินกสฺ.วัสุดฺ.ทาร.แก่ข้าราชการ");
+result = r.Calculate(".เงินกสฺ.วัสุดฺ.ทาร.แก่ข้าราชการ", "เงินกู้สวัสดิการแก่ข้าราชการ");
+result = r.Calculate("4.สินเชื่อ", "4..สิน!■ชื่อ.");
+result = r.Calculate("สินเชื่อ", ".สิน!■ชื่อ.");
+result = r.Calculate("5.สินเชื่อ", "5.ลัน!■ซื่ซี");
+result = r.Calculate("สินเชื่อ", "ลัน!■ซื่ซี");
+
+//Console.Write($"{Environment.NewLine}Press any key to exit...");
+//Console.ReadKey(true);
+
---
+++ cseditdistance/RunTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cseditdistance
+{
+  public class RunTest
+  {
+    public int Calculate(string source1, string source2) //O(n*m)
+    {  
+      int return_int = 0;
+      var currentDate = DateTime.Now;
+
+      return_int = LevenshteinDistance.Calculate(source1, source2);
+
+      Console.WriteLine($"{currentDate:d}-{currentDate:t}: Result = {return_int} #: When text1 = {source1} , text2 = {source2}");
+      return return_int;
+     }
+  }
+}
