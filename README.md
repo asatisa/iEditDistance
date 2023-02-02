15a6fcc1211ce9fcc8899be9332f6b597bd80956
asatisa <asatisa.wang@gmail.com>
2023-02-02 16:17:37 +0700

    1st add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # iEditDistance
+
+Referece Doc:
+"Edit Distance" เป็นอัลกอลิทึมที่ใช้การหาคำที่สะกดใกล้เคียง
+
+https://gist.github.com/Davidblkx/e12ab0bb2aff7fd8072632b396538560
+
+https://stackoverflow.com/questions/9453731/how-to-calculate-distance-similarity-measure-of-given-2-strings
---
+++ cseditdistance/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+//var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"{Environment.NewLine}On {currentDate:d} at {currentDate:t}!");
+
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
