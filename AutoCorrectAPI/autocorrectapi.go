f3652268f18abb3f37be2b38181b12f2e45d20a8
atthapol@lnv11k <atthapol@lnv11k>
2023-02-16 08:17:33 +0700

    Update source code and exe version 1. = DEBUG exe version 1.02 = Without DEBUG

--- AutoCorrectAPI/autocorrectapi.go
+++ AutoCorrectAPI/autocorrectapi.go
@@ -20,8 +20,20 @@ import (
 )
 
 // ************ Start: Constant of Application ************//
-const app_version string = "0.9.230214.01"
-const app_comment string = "Alpha version"
+const app_version string = "1.0.230216.02"
+const app_comment string = "Beta version: remove debugmode"
+const max_excel_rows int = 500
+
+type debug_mode int
+
+const (
+	mode_none = iota //do nothing
+	mode_default
+	mode_deep_dive
+)
+
+const iDEBUG_MODE debug_mode = mode_none //iDEBUG_MODE_DEFAULT
+const isINFO_MODE = true
 
 //const prefixExcelCompareFileName = "./Data_Compare_" //.xlsx"
 //************ End: Constant of Application ************//
@@ -75,6 +87,7 @@ var helloworld = []mappingValue{
 	{KEY: "Greeting", Value: "Hello! World."},
 }
 
+// TEST DATA
 var autoCorrects = []autocorrectAllData{
 	{Topic: "ชื่อบริษัท", Data: "APPLA", MinPercentMatch: "80"},
 	{Topic: "TAXID-สัญญารายย่อย", Data: "1999892001002", MinPercentMatch: "90"},
@@ -82,15 +95,15 @@ var autoCorrects = []autocorrectAllData{
 }
 
 var inputDistanceCorrects = []autocorrectInput{
-	{Topic: "ชื่อบริษัท", Data: "sitting", MinPercentMatch: "80"},
+	{Topic: "-", Data: "-", MinPercentMatch: "-"},
 }
 
 var outputDistanceCorrects = []autocorrectAllData{
-	{Topic: "ชื่อบริษัท", Data: "sitting", MinPercentMatch: "80", ResultCode: 0, ResultMessage: "Success", ResultData: "sitting", ResultPercentMatch: "80"},
+	{Topic: "-", Data: "-", MinPercentMatch: "-", ResultCode: 0, ResultMessage: "-", ResultData: "-", ResultPercentMatch: "-"},
 }
 
 var outputDistanceCorrectsLatest = []autocorrectAllData{
-	{Topic: "ชื่อบริษัท", Data: "sitting", MinPercentMatch: "80", ResultCode: 0, ResultMessage: "Success", ResultData: "sitting", ResultPercentMatch: "80"},
+	{Topic: "-", Data: "-", MinPercentMatch: "-", ResultCode: 0, ResultMessage: "-", ResultData: "-", ResultPercentMatch: "-"},
 }
 
 //************ End: Variable and declaration of Application ************//
@@ -199,7 +212,7 @@ func getExcelFileName(inputTopic string) string {
 	original_excelCompareFileName := readINI("config", "excel_compare_filename")
 	excel_filename := strings.Replace(original_excelCompareFileName, "{%TOPIC%}", inputTopic, -1)
 	fmt.Println("Original Excel Compare Filename: " + original_excelCompareFileName)
-	fmt.Println("Excel Compare Filename: " + excelCompareFileName)
+	//fmt.Println("Excel Compare Filename: " + excelCompareFileName)
 	return excel_filename
 }
 
@@ -210,10 +223,27 @@ func GetExcelCompanyValue(excel_filename string, axis string) string {
 		return "N_A"
 	}
 
-	// cellVal := f.GetCellValue("Sheet1", "A1")
 	cellVal := f.GetCellValue("Sheet1", axis)
-	fmt.Println("cell value = " + cellVal)
+	s := "--------------------------------------------------------------------------------------\n" +
+		"		Get cellvalue = " + cellVal
+	printDebug(mode_deep_dive, s)
 	return cellVal
+}
+
+func printDebug(mode debug_mode, a ...any) {
+	if iDEBUG_MODE <= mode_none {
+		return // do nothing
+	}
+
+	if iDEBUG_MODE >= mode {
+		fmt.Println(a...)
+	}
+}
+
+func printInfo(a ...any) {
+	if isINFO_MODE {
+		fmt.Println(a...)
+	}
 }
 
 func CountRowsExcelCompanyValue(excel_filename string) int {
@@ -223,14 +253,18 @@ func CountRowsExcelCompanyValue(excel_filename string) int {
 		return -1
 	}
 
-	var i_loop int = 0
+	//var i_loop int = 0
+	var count_rows int = 0
 	var axis = ""
-	fmt.Println("First init = ", i_loop)
-	for i_loop := 1; i_loop <= 200; i_loop++ {
+	printInfo("Excel Compare Filename: " + excel_filename)
+	printInfo("		Count all rows of excel")
+	for i_loop := 1; i_loop <= max_excel_rows; i_loop++ {
+		count_rows = i_loop - 1
 		axis = fmt.Sprintf("A%d", i_loop)
 		cellVal := f.GetCellValue("Sheet1", axis)
 		if cellVal == "" {
-			return i_loop - 1
+			printInfo("		Count rows = ", count_rows)
+			return count_rows
 		}
 	}
 
@@ -245,7 +279,8 @@ func getDistanceCorrect(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, outputDistanceCorrects)
 }
 
-func postDistanceCorrect(c *gin.Context) {
+// Distance Correct Version 0.9
+func postDistanceCorrectV0_9(c *gin.Context) {
 	var inputJson autocorrectAllData
 
 	if err := c.BindJSON(&inputJson); err != nil {
@@ -268,6 +303,7 @@ func postDistanceCorrect(c *gin.Context) {
 	//source1 := "kitten"       //source data.
 	source2 := inputJson.Data //input data from client req. //"sitting"
 	i_count = i_count + 1
+	i_count_loop_req = 0
 
 	// Start ---- 1. Check database file or excel dictionary is Exists
 	local_excel_filename = getExcelFileName(inputTopic)
@@ -293,7 +329,7 @@ func postDistanceCorrect(c *gin.Context) {
 
 	// Start ---- 2. Compare data in  loop from source vs excel file
 	var axis = ""
-	fmt.Println("First init = 0")
+	printDebug(mode_default, "		First init = 0")
 	var current_max_percent_result float32 = 0.0
 
 	var max_loop int = CountRowsExcelCompanyValue(local_excel_filename)
@@ -307,7 +343,8 @@ func postDistanceCorrect(c *gin.Context) {
 		result_distance = 0
 		result_percent = 0.0
 
-		fmt.Println("Loop: [", i_excel_current_row, "]: DistanceCorrect: of ", source1, ", ", source2)
+		printDebug(mode_deep_dive, "Loop: [", i_excel_current_row, "]: DistanceCorrect: of ", source1, ", ", source2)
+		fmt.Println()
 		result_distance, result_percent = CalculateDistance(source1, source2)
 		result_percent_string = fmt.Sprintf("%f", result_percent) //convert to string
 
@@ -357,6 +394,134 @@ func postDistanceCorrect(c *gin.Context) {
 	}
 }
 
+// Distance Correct Version 1
+// Update: 2023-02-16
+func postDistanceCorrect(c *gin.Context) {
+	var InputJsonOriginal autocorrectInput
+	var inputJsonAllData autocorrectAllData
+
+	if err := c.BindJSON(&InputJsonOriginal); err != nil {
+		return
+	}
+
+	var result_distance int = 0
+	var outputJson []autocorrectAllData
+	var result_percent float32 = 0
+	var result_percent_string string
+	var result_message string = ""
+	var local_excel_filename string = ""
+	var isFindDistanceFound bool = false
+
+	var inputTopic string = InputJsonOriginal.Topic
+	var inputData string = InputJsonOriginal.Data
+	var inputMinPercentMatch string = InputJsonOriginal.MinPercentMatch
+	//inputJsonAllData = `{"Topic": "", "Data": "", "MinPercentMatch": "", ResultCode: -999, ResultMessage: "", ResultData: "", ResultPercentMatch: ""}`
+	inputJsonAllData.Topic = inputTopic
+	inputJsonAllData.Data = inputData
+	inputJsonAllData.MinPercentMatch = inputMinPercentMatch
+	inputJsonAllData.ResultCode = -999
+	inputJsonAllData.ResultMessage = ""
+	inputJsonAllData.ResultPercentMatch = ""
+	inputJsonAllData.ResultPercentMatch = "-999"
+
+	//inputJsonAllData = append(inputJsonAllData, inputJsonAllDataBuffer)
+
+	floatMinPercentMatch, _ := strconv.ParseFloat(inputMinPercentMatch, 64)
+
+	//source1 := "kitten"       //source data.
+	source2 := inputJsonAllData.Data //input data from client req. //"sitting"
+	i_count = i_count + 1
+	i_count_loop_req = 0
+
+	// Start ---- 1. Check database file or excel dictionary is Exists
+	local_excel_filename = getExcelFileName(inputTopic)
+	//fmt.Println("Check database file or excel dictionary is Exists")
+	if fileExist(local_excel_filename) == false {
+		result_message = "Error. Find Database/Excel not found!"
+		outputJson = []autocorrectAllData{
+			{Topic: inputTopic,
+				Data:               inputData,
+				MinPercentMatch:    inputMinPercentMatch,
+				ResultCode:         -999, //Failure
+				ResultMessage:      result_message,
+				ResultData:         "",
+				ResultPercentMatch: "0"},
+		}
+		fmt.Println("Error! " + result_message)
+		outputDistanceCorrectsLatest = nil
+		outputDistanceCorrectsLatest = append(outputDistanceCorrectsLatest, inputJsonAllData)
+		c.IndentedJSON(http.StatusCreated, outputJson)
+		return //exit function
+	}
+	// Finish ---- 1. Check database file or excel dictionary is Exists
+
+	// Start ---- 2. Compare data in  loop from source vs excel file
+	var axis = ""
+	printInfo("		First init = 0")
+	var current_max_percent_result float32 = 0.0
+
+	var max_loop int = CountRowsExcelCompanyValue(local_excel_filename)
+	for i_excel_current_row := 1; i_excel_current_row <= max_loop; i_excel_current_row++ {
+		axis = fmt.Sprintf("A%d", i_excel_current_row)
+		source1 := GetExcelCompanyValue(local_excel_filename, axis)
+
+		// Clear a variables
+		result_message = ""
+		outputJson = nil
+		result_distance = 0
+		result_percent = 0.0
+
+		result_distance, result_percent = CalculateDistance(source1, source2)
+		result_percent_string = fmt.Sprintf("%f", result_percent) //convert to string
+		//printDebug("		Loop: [", i_excel_current_row, "]: Distance Correct: of ", source1, ", ", source2, ", Result = ", result_percent_string, "%")
+
+		// Condition is pass
+		if result_percent >= float32(floatMinPercentMatch) {
+			outputDistanceCorrects = append(outputDistanceCorrects, inputJsonAllData)
+			isFindDistanceFound = true
+			result_message = fmt.Sprintf("Found: data distance = %d", result_distance)
+			printDebug(mode_deep_dive, "		FOUND! row = ", i_excel_current_row, result_message)
+
+			if result_percent > current_max_percent_result {
+				current_max_percent_result = result_percent
+				outputJson = []autocorrectAllData{
+					{Topic: inputTopic,
+						Data:               inputData,
+						MinPercentMatch:    inputMinPercentMatch,
+						ResultCode:         0, //Success
+						ResultMessage:      result_message,
+						ResultData:         source1,
+						ResultPercentMatch: result_percent_string},
+				}
+				outputDistanceCorrectsLatest = nil
+				outputDistanceCorrectsLatest = outputJson
+				//c.IndentedJSON(http.StatusCreated, outputJson)
+			}
+		}
+	}
+	// Finish ---- 2. Compare data in  loop from source vs excel file
+
+	if isFindDistanceFound { // find found
+		c.IndentedJSON(http.StatusCreated, outputDistanceCorrectsLatest)
+		return
+	} else { // find not found
+		result_message = "Find data mapping not found"
+		outputJson = []autocorrectAllData{
+			{Topic: inputTopic,
+				Data:               inputData,
+				MinPercentMatch:    inputMinPercentMatch,
+				ResultCode:         -1, //Failure
+				ResultMessage:      result_message,
+				ResultData:         "",
+				ResultPercentMatch: "0"},
+		}
+		outputDistanceCorrectsLatest = nil
+		//outputDistanceCorrectsLatest = append(outputDistanceCorrectsLatest, inputJsonAllData)
+		c.IndentedJSON(http.StatusCreated, outputJson)
+		return
+	}
+}
+
 func runRestAPI() {
 	router := gin.Default()
 
@@ -399,18 +564,20 @@ func runTestDistance() int {
 	return 0
 }
 
+var i_count_loop_req int = 0
+
 func CalculateDistance(source1 string, source2 string) (distance int, percent float32) {
 	//var result_run int = 0
 	var source1Length int
 	var source2Length int
 	var maxLength int = 0
 
-	i_count = i_count + 1
+	i_count_loop_req = i_count_loop_req + 1
 	resultPercent = 0
 	distance = 0
 
-	fmt.Println("--------------------------------------------------------------------------------------")
-	fmt.Println(i_count, " : Calculation UTF8 Distance of: source1 = ", source1, ", source2 = ", source2)
+	printDebug(mode_default, "--------------------------------------------------------------------------------------")
+	printDebug(mode_default, "[", i_count, "]", i_count_loop_req, " : Calculation UTF8 Distance of: source1 = ", source1, ", source2 = ", source2)
 
 	// Length of UTF-8 encoded string
 	source1Length = utf8.RuneCountInString(source1) //len(source1)
@@ -427,9 +594,7 @@ func CalculateDistance(source1 string, source2 string) (distance int, percent fl
 	distance = levenshtein.ComputeDistance(source1, source2)
 	resultPercent = ToFloat32(maxLength-distance) / ToFloat32(maxLength) * 100
 
-	fmt.Println(i_count, " : When data : source1 = ", source1, ", source2 = ", source2)
-	fmt.Println(i_count, " :             distance = ", distance, ", maxchar = ", maxLength, ", percent = ", resultPercent, " %")
-	//fmt.Printf("%d : Result = %d, %d, %f %% #", i_count, return_int, maxLength, percent)
+	printDebug(mode_default, "[", i_count, "]", i_count_loop_req, " :             distance = ", distance, ", maxchar = ", maxLength, ", percent = ", resultPercent, " %")
 
 	return distance, resultPercent
 }
